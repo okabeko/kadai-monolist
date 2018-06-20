0090b775b1187220ed60cd86d3eb635bd37dfbeb
okabeko <2minus.plus2@gmail.com>
2018-06-20 11:45:05 +0000

    want

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,4 +10,19 @@ class ApplicationController < ActionController::Base
       redirect_to login_url
     end
   end
+  
+  def read(result)
+    code = result['itemCode']
+    name = result['itemName']
+    url = result['itemUrl']
+    # 画像URL末尾に含まれる文字列を削除（元画像の取得）
+    image_url = result['mediumImageUrls'].first['imageUrl'].gsub('?_ex=128x128', '')
+    
+    return {
+      code: code,
+      name: name,
+      url: url,
+      image_url: image_url,
+    }
+  end
 end
---
+++ app/controllers/items_controller.rb
@@ -0,0 +1,30 @@
+class ItemsController < ApplicationController
+  before_action :require_user_logged_in
+  
+  def new
+    # @itemsをカラの配列として初期化
+    @items = []
+    
+    # フォームから送信される検索ワードの取得
+    @keyword = params[:keyword]
+    if @keyword.present? #
+      results = RakutenWebService::Ichiba::Item.search({
+        keyword: @keyword,
+        imageFlag: 1,
+        hits: 20,
+      })
+      
+      # resultsからItemモデルのインスタンスを作成
+      results.each do |result|
+        item = Item.find_or_initialize_by(read(result))
+        # itemを[]に追加
+        @items << item
+      end
+    end
+  end
+  
+  def show
+    @item = Item.find(params[:id])
+    @want_users = @item.want_users
+  end
+end
